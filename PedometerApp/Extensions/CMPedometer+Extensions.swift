703336d5817b9eefa256ca7dac255f87459e7f2e
Albert Pangestu <stamina.left@gmail.com>
2022-01-10 19:56:56 +0700

    Created CMPedometer that conforms Pedometer protocol.

---
+++ PedometerApp/Extensions/CMPedometer+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CMPedometer+Extensions.swift
+//  PedometerApp
+//
+//  Created by Albert Pangestu on 10/01/22.
+//
+
+import Foundation
+import CoreMotion
+
+extension CMPedometer: Pedometer {
+    
+    func startPedometer() {
+        <#code#>
+    }
+}
